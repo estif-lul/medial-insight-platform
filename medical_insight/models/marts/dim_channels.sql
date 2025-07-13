32450a0658f65399c7eaa9f99ca8371f9160e5dc
est-lul <estlul2004@gmail.com>
2025-07-13 13:25:06 +0300

    Updated some DBT models

--- medical_insight/models/marts/dim_channels.sql
+++
@@ -1,8 +0,0 @@
-SELECT DISTINCT
-    channel_name AS channel_id,
-    AVG(LENGTH(text)) AS avg_message_length,
-    AVG(CASE WHEN has_image THEN 1 ELSE 0 END) AS image_ratio,
-    MIN(message_date) AS first_seen,
-    MAX(message_date) AS last_seen
-FROM {{ ref('stg_telegram_messages') }}
-GROUP BY channel_name
