2618b1dbf26bfee77134032adccb51c40f61ce26
est-lul <estlul2004@gmail.com>
2025-07-13 13:19:50 +0300

    Added screenshot for API

--- medical_insight/models/marts/dim_channels.sql
+++ medical_insight/models/marts/dim_channels.sql
@@ -1,5 +1,8 @@
 SELECT DISTINCT
     channel_name AS channel_id,
-    MIN(message_date) AS first_seen
+    AVG(LENGTH(text)) AS avg_message_length,
+    AVG(CASE WHEN has_image THEN 1 ELSE 0 END) AS image_ratio,
+    MIN(message_date) AS first_seen,
+    MAX(message_date) AS last_seen
 FROM {{ ref('stg_telegram_messages') }}
 GROUP BY channel_name
